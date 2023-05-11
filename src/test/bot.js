cd694001c52482dc42cee7b7514a69b944378758
Andrew <ac@i-ns.net>
2023-05-11 15:13:22 +0300

    acpush by qaptivator

--- src/test/bot.js
+++ src/test/bot.js
@@ -1,9 +1,9 @@
 import { Cohandler } from "../index.js";
 import { Client, GatewayIntentBits } from 'discord.js';
 
-const client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
+let client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
 
-const handler = new Cohandler(
+let handler = new Cohandler(
     client, 
     null, 
     {
